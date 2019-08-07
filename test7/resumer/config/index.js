e6c8c76713e2f2b5d1467ec49d2f616173e78fb6
vincent <670700452@qq.com>
2019-08-07 20:17:25 +0800

    添加按钮及完善导航栏

--- test7/resumer/config/index.js
+++ test7/resumer/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/Vue-demo/test6/resumer/dist',
+    assetsPublicPath: '/Vue-demo/test7/resumer/dist',
 
 
 

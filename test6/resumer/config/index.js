59e4e3ff73150a50a93fb979d89a69ba076c8ea8
vincent <670700452@qq.com>
2019-08-07 15:36:03 +0800

    resume-tool

--- test6/resumer/config/index.js
+++ test6/resumer/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'test6/resumer/dist',
+    assetsPublicPath: '/test6/resumer/dist',
 
 
     /**
